824f8c844e45694632acd821e22b957a579ae8c4
TaHa Hammouz <91549817+TahaHammouz12@users.noreply.github.com>
2022-03-14 12:13:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+
+dotenv.config({ path: './config.env' });
+
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
+});
+
 db.connect((error) => {
   if (error) {
     console.log(error)
